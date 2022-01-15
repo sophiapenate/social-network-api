6c5ef5e633a471dc0957bf95c57184ba3585ae44
Sophia Barrett <sophia@yoursummit.media>
2022-01-14 17:26:25 -0700

    initial thought routes

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { Thought } = require("../models");
+
+const thoughtController = {
+    getAllThoughts() {},
+
+    getThoughtById() {},
+
+    createThought() {},
+
+    updateThought() {},
+
+    deleteThought() {}
+};
+
+module.exports = thoughtController;
--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
