e09ee60e3201a64cdd2030300ce4b7448f335b29
Sophia Barrett <sophia@yoursummit.media>
2022-01-14 17:50:53 -0700

    add initial thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,15 +1,92 @@
-const { Thought } = require("../models");
+const { Thought, User } = require("../models");
 
 const thoughtController = {
-    getAllThoughts() {},
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-    getThoughtById() {},
+  getThoughtById({ params }, res) {
+    Thought.findById(params.id)
+      .then((dbData) => {
+        if (!dbData) {
+          res
+            .status(404)
+            .json({ message: `No thought found with id ${params.id}!` });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-    createThought() {},
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { username: body.username },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbData) => {
+        if (!dbData) {
+          res
+            .status(404)
+            .json({ message: `No user found with username ${body.username}!` });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-    updateThought() {},
+  updateThought({ params, body }, res) {
+    Thought.findByIdAndUpdate(params.id, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbData) => {
+        if (!dbData) {
+          res
+            .status(404)
+            .json({ message: `No thought found with id ${params.id}!` });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
-    deleteThought() {}
+  deleteThought({ params }, res) {
+    Thought.findByIdAndDelete(params.id)
+      .then((dbData) => {
+        if (!dbData) {
+          res
+            .status(404)
+            .json({ message: `No thought found with id ${params.id}!` });
+          return;
+        }
+        res.json({ message: "Thought successfully deleted!" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
 
 module.exports = thoughtController;
--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
