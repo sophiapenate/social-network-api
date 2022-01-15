92d14d6ed93bf5c3bd2fbded660effc1b301581d
Sophia Barrett <sophia@yoursummit.media>
2022-01-15 12:45:55 -0700

    reformat date

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,14 +1,20 @@
 const formatDate = (date) => {
-  const options = {
-    hour: "numeric",
-    minute: "numeric",
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  };
+  const nth = d => {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1: return 'st';
+      case 2: return 'st'; 
+      case 3: return 'st'; 
+      default: return 'th'; 
+    }
+  }
 
-  return new Date(date).toLocaleString("en-us", options);
+  const month = new Date(date).toLocaleString("en-us", { month: "short" });
+  const day = new Date(date).toLocaleString("en-us", { day: "numeric" });
+  const year = new Date(date).toLocaleString("en-us", { year: "numeric" });
+  const time = new Date(date).toLocaleString("en-us", { hour: "numeric", minute: "numeric" }).toLowerCase();
+
+  return `${month} ${day + nth(day)}, ${year} at ${time}`;
 };
 
 module.exports = formatDate;
